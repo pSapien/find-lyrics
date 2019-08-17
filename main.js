d0e563f27656c45f13c1bfda069b953e588b192c
Prateek Thapa <prateekthapa26@gmail.com>
2019-08-17 17:51:02 +0545

    added song and artist from the command line

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const getLyrics = require('./getLyrics');
 
 (async function main() {
   try {
-    const lyrics = await getLyrics('Unsainted', 'Slipknot');
+    const commandLineArgs = process.argv.slice(2);
+    const [song, artist] = commandLineArgs;
+
+    const lyrics = await getLyrics(song, artist);
     console.log(lyrics);
   } catch (err) {
     console.log(err);
