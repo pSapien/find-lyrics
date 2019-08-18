6ee3ce707197e8a1f0d325550dfbe6946525e3c2
Prateek Thapa <prateekthapa26@gmail.com>
2019-08-18 09:56:32 +0545

    add log

---
+++ log.js
@@ -0,0 +1,40 @@
+const boxen = require('boxen');
+const chalk = require('chalk');
+
+const { version, description, name } = require('./package.json');
+
+class Log {
+  static packageName() { 
+    console.log(
+      boxen(
+        chalk.cyan(
+          `${name} v${version} - ${description}`
+        ),
+        { padding: 1, margin: 1, borderStyle: 'double' }
+      )
+    );
+  }
+
+  static songAndArtist(song, artist) { 
+    console.log('\n');
+    console.log(chalk.cyanBright(`The lyrics of ${song} by ${artist}`));
+  }
+
+  static lyrics(lyrics) { 
+    console.log('\n');
+    console.log(lyrics);
+  }
+
+  static error() { 
+    console.log(
+      chalk.red(`
+      This can happen because of two reasons
+          1: The song is not found.
+          2: There's a spelling mistake in artist or song.
+      Please make sure you have enter your inputs correctly. Thanks!
+      `)
+    );
+  }
+}
+
+module.exports = Log;
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const boxen = require('boxen');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 
-const package = require('./package.json');
+const Log = require('./log');
 const getLyrics = require('./getLyrics');
 
 const questions = [
@@ -16,32 +16,16 @@ const spinner = ora('Searching your lyrics');
 
 (async function main() {
   try {
-    console.log(
-      boxen(
-        chalk.cyan(
-          `Find Lyrics v${package.version} - Search lyrics on your terminal`
-        ),
-        { padding: 1, margin: 1, borderStyle: 'double' }
-      )
-    );
-
+    Log.packageName();
+  
     const { song, artist } = await inquirer.prompt(questions);
     spinner.start();
     const lyrics = await getLyrics(song, artist);
     spinner.stop();
 
-    console.log('\n');
-    console.log(chalk.cyanBright(`The lyrics of ${song} by ${artist}`));
-    console.log('\n');
-    console.log(lyrics);
+    Log.songAndArtist(song, artist);
+    Log.lyrics(lyrics);
   } catch (err) {
-    console.log(
-      chalk.red(`
-      This can happen because of two reasons
-          1: The song is not found.
-          2: There's a spelling mistake in artist or song.
-      Please make sure you have enter your inputs correctly. Thanks!
-      `)
-    );
+    Log.error();
   }
 })();
